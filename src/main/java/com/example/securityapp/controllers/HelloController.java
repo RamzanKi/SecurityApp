0e9f0856e9e081fe4b544c2041a9e6be8a8373e9
ramzankikaev <sqrcloud@gmail.com>
2023-02-16 13:24:51 +0300

    Initial commit

---
+++ src/main/java/com/example/securityapp/controllers/HelloController.java
@@ -0,0 +1,44 @@
+package com.example.securityapp.controllers;
+
+import com.example.securityapp.config.SecurityConfig;
+import com.example.securityapp.security.PersonDetails;
+import com.example.securityapp.services.AdminService;
+import com.example.securityapp.services.PersonDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
+        return "hello";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        adminService.doAdminStuff();
+        return "admin";
+    }
+}
---
+++ src/main/java/com/example/securityapp/services/PersonDetailsService.java
@@ -0,0 +1,34 @@
+package com.example.securityapp.services;
+
+import com.example.securityapp.models.Person;
+import com.example.securityapp.repositories.PeopleRepository;
+import com.example.securityapp.security.PersonDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PersonDetailsService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Person> person = peopleRepository.findByUsername(username);
+
+        if (person.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return new PersonDetails(person.get());
+    }
+}
